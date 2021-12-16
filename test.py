272529f30dbfb5ed6d8f1b36bc0b3c8c1d5c1c55
younesbelkada <younesbelkada@gmail.com>
2021-12-16 14:47:51 +0100

    add train code

--- test.py
+++ test.py
@@ -7,8 +7,11 @@ from PosTER.dataset import DynamicDataset, StaticDataset, my_collate
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-data = StaticDataset(config)
-dataloader = DataLoader(data, batch_size=2, collate_fn=my_collate)
+train_data = StaticDataset(config, 'train')
+train_dataloader = DataLoader(train_data, batch_size=config['Training']['batch_size'], collate_fn=my_collate)
+
+val_data = StaticDataset(config, 'val')
+val_dataloader = DataLoader(val_data, batch_size=config['Training']['batch_size'], collate_fn=my_collate)
 
 trainer = Trainer(config)
-trainer.train(dataloader, dataloader)
+trainer.train(train_dataloader, val_dataloader)
