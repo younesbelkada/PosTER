Fixed normalization

PosTER/Datasets/augmentations.py
@@ -15,13 +15,16 @@ class NormalizeKeypoints(object):
         self.n = max(im_size)
     def __call__(self, keypoints):
         if torch.is_tensor(keypoints):
-            keypoints_xyc = convert_xyc(keypoints)
+            keypoints_xyc = convert_xyc(keypoints.unsqueeze(0))
         else:
             keypoints_xyc = convert_xyc_numpy(keypoints)
         normalized_X = np.array(keypoints_xyc[0])/self.n
         normalized_Y = np.array(keypoints_xyc[1])/self.n
         C = np.array(keypoints_xyc[2])
-        return convert_keypoints_batch([normalized_X, normalized_Y, C])
+        if len(normalized_X.shape) == 2:
+            return convert_keypoints_batch([normalized_X, normalized_Y, C])
+        else:
+            return convert_keypoints([normalized_X, normalized_Y, C]).unsqueeze(0)
 
 class NormalizeKeypointsRelative(object):
     """
@@ -138,7 +141,7 @@ class BodyParts(object):
     def __init__(self):
         pass 
     def __call__(self, keypoints):
-        return torch.stack(torch.split(keypoints, 3, dim=1), dim=1)
+        return torch.stack(torch.split(keypoints, 3, dim=1), dim=1).squeeze(0)
 
 class RandomMask(object):
     """